0b535e6cc6ce72defd32eb707e03ac84ec317b12
Buck Perley <buck@eggplantdigital.cn>
2017-01-22 21:27:18 -0800

    fixed typo

--- micropayment_server/settings.py
+++ micropayment_server/settings.py
@@ -42,7 +42,7 @@ DEBUG = True
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
